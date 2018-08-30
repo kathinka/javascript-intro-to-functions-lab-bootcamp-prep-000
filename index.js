23d2ab4855765c2d9c84801dae188a7ea985cc73
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-08-30 16:59:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+function shout(string){
+return string.toUpperCase()
+
+}
+
+function logShout(string){
+  console.log('${shout}')
+}
+
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+
+function logwhisper(string){
+  console.log("hello")
+}
+
+
+function sayHiToGrandma(string === string.lowerCase){
+  return "I can´t hear you!"
+}
