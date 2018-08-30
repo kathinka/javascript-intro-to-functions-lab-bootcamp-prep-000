a5a90a8db0bd1006765f3423ad889d200569f57a
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-08-30 17:10:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,14 @@ function logWhisper(string){
 }
 
 
-function sayHiToGrandma(string) {
-  if (string=== string.toLowerCase()){
+function sayHiToGrandma (string) {
+  if (string === string.toLowerCase()){
   return "I can´t hear you!"
-}}
+  }
+  else if (string === string.toUpperCase()){
+    return "YES INDEED!"
+}
+  else if (string === "I love you, Grandma"){
+    return "I love you, too"
+  }
+}
