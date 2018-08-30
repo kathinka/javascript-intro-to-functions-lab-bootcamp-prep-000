9ed42e087a0101c7647d9a5a022903459b0764f5
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-08-30 17:14:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ function sayHiToGrandma (string) {
   return ("I can't hear you!")
   }
   else if (string === string.toUpperCase()){
-    return "YES INDEED!"
+    return ("YES INDEED!")
 }
   else if (string === "I love you, Grandma"){
-    return "I love you, too."
+    return ("I love you, too.")
   }
 }
